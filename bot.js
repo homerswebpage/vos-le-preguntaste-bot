4b8e3b2fa3da3c24f3ab33d33775c1cd25894c24
Daniel Marconi <homerswebpage24@gmail.com>
2021-01-26 19:45:38 -0300

    responder imagen

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 import Twitter from 'twitter';
+import fs from 'fs';
 
 const client = new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
@@ -7,15 +8,23 @@ const client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
+const IMGID = '1354190969508405250';
+
 export const botScreenName = process.env.BOT_SCREEN_NAME;
+
+export const searchTweets = (q) => {
+    client.get('search/tweets', { q: q }, function (error, tweets, response) {
+        console.log(tweets);
+    });
+};
 
 export const tweetIt = (tweetText) => {
     console.log(`${botScreenName} will tweet "${tweetText}"`);
     client.post('statuses/update', {
         status: tweetText
     })
-    .then((tweet) => console.log('Success!'))
-    .catch((err) => console.log('Error', err));
+        .then((tweet) => console.log('Success!', tweet))
+        .catch((err) => console.log('Error', err));
 };
 
 export const replyTweet = (replyText, from, tweetId) => {
@@ -25,8 +34,20 @@ export const replyTweet = (replyText, from, tweetId) => {
         status: `@${from} ${replyText}`,
         in_reply_to_status_id: tweetId
     })
-    .then((tweet) => console.log(`Reply to ${tweetId} from @${from} was successful`))
-    .catch((err) => console.log(`Error trying to reply ${tweetId} from @${from}`, err));
+        .then((tweet) => console.log(`Reply to ${tweetId} from @${from} was successful`))
+        .catch((err) => console.log(`Error trying to reply ${tweetId} from @${from}`, err));
+};
+
+export const replyMedia = (imgId, from, tweetId) => {
+    console.log(`${botScreenName} will reply the tweet ${tweetId} from @${from}`);
+
+    client.post('statuses/update', {
+        status: `@${from}`,
+        in_reply_to_status_id: tweetId,
+        media_ids: imgId
+    })
+        .then((tweet) => console.log(`Reply to ${tweetId} from @${from} was successful`))
+        .catch((err) => console.log(`Error trying to reply ${tweetId} from @${from}`, err));
 };
 
 export const replyMentions = () => {
@@ -41,17 +62,32 @@ export const replyMentions = () => {
             const from = tweet.user.screen_name;
             const text = tweet.text;
             const replyTo = tweet.in_reply_to_screen_name; // persona arrobada en el tweet inicial, en este caso el @ del bot
-            
+
             console.log(`[ID: ${tweetId}] ${from} (replying to ${replyTo}) twitted: ${text}`);
 
-            if (replyTo === botScreenName) {
-                replyTweet('mmm no, la verdad que no maquina', from, tweetId);
-            }    
-    
+            if (text.includes(`@${botScreenName}`)) {
+                replyMedia(IMGID, from, tweetId);
+            }
+
         });
-    
+
         stream.on('error', function (error) {
             console.log('Stream error', error);
         });
     });
 };
+
+export const uploadMedia = (fileName) => {
+    const data = fs.readFileSync(fileName);
+
+    client.post('media/upload', { media: data }, function (error, media, response) {
+
+        if (!error) {
+
+            console.log(media);
+
+        } else {
+            console.log(error);
+        }
+    });
+}
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import { botScreenName, replyMentions } from './bot.js';
+import { botScreenName, replyMentions, searchTweets, tweetIt, uploadMedia } from './bot.js';
 
 console.log(`${botScreenName} is running`);
 
 replyMentions();
+
+//getTweet('1344070510276206594');
+
+//searchTweets('Jaja esta foto solo la creen los kumpas!! Porque fue el ministro? cuando tendria que haberlo echo una enfermera o su medico??');
+
+//tweetIt('asdfg');
+
+
+//uploadMedia('./img/eumessi.jpg');
